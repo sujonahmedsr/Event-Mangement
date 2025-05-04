a1e8b1876d3df0586a60d1a676facbf807b2cb0e
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2025-05-04 21:22:24 +0600

    add need files

--- event_backend/src/app.ts
+++ event_backend/src/app.ts
@@ -1,28 +1,29 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from "cors"
 import cookieParser from "cookie-parser"
-import status from 'http-status';
+import globalErrorHandler from './middlewares/globalErrorHandler';
+import notFound from './middlewares/notFound';
+import router from './router';
 const app: Application = express();
 
+// middlewares 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send({
-      Message: "Event Management server..",
-    });
+  res.send({
+    Message: "Event Management server..",
   });
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(status.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!",
-        },
-    });
 });
+
+// all routes 
+app.use("/api/v1", router);
+
+// Global Error Handler 
+app.use(globalErrorHandler)
+
+// Api not found 
+app.use(notFound);
 
 export default app;
